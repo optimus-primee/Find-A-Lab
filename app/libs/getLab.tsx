1c1b6adee888bfa805e04f3bd34f871946a16b95
Your Name <okhuomonajayi54@gmail.com>
2023-12-19 07:44:49 +0100

    fix

--- app/libs/getLab.tsx
+++ app/libs/getLab.tsx
@@ -1,9 +1,12 @@
 export default async function getLab(labId: string) {
-  const res = await fetch(`http://localhost:3031/labs/${labId}`,{
-    next:{
-      revalidate:0
+  const res = await fetch(
+    `https://json-find-a-lab.onrender.com/labs/${labId}`,
+    {
+      next: {
+        revalidate: 0,
+      },
     }
-  });
+  );
 
   if (!res.ok) throw new Error("failed to fetch data");
 
